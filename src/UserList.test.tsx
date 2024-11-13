4dc1a6c86b014217fc40bcfc6640b792f3c5ad40
Amirhosein Torabi <ah.torabi@rayanhamafza.com>
2024-11-13 12:46:21 +0330

    define renderComponent function for making it reusable

--- src/UserList.test.tsx
+++ src/UserList.test.tsx
@@ -2,27 +2,30 @@ import { render, screen, within } from "@testing-library/react";
 import { describe, it, expect, vi } from "vitest";
 import { UserLists } from "./UserLists";
 
+function renderComponent() {
+  const users = [
+    { name: "amir", email: "amir@gmail.com" },
+    { name: "hosein", email: "test@test.com" },
+  ];
+  render(<UserLists users={users} />);
+
+  return { users };
+}
+
 describe("userlist component test", () => {
   it("Should render one row per users", async () => {
-    const users = [
-      { name: "amir", email: "amir@gmail.com" },
-      { name: "hosein", email: "test@test.com" },
-    ];
-    const { container } = render(<UserLists users={users} />);
-
+    renderComponent();
     // screen.logTestingPlaygroundURL();
 
-    const rows = container.querySelectorAll("tbody tr");
+    // Following approach does not have container
+    // const rows = container.querySelectorAll("tbody tr");
+    // User instead the within
+    const rows = within(screen.getByTestId("users")).getAllByRole("row");
     expect(rows).toHaveLength(2);
   });
 
   it("render the email and name of each user", () => {
-    const users = [
-      { name: "amir", email: "amir@gmail.com" },
-      { name: "hosein", email: "test@test.com" },
-    ];
-    render(<UserLists users={users} />);
-
+    const { users } = renderComponent();
     for (const user of users) {
       const name = screen.getByRole("cell", { name: user.name });
       const email = screen.getByRole("cell", { name: user.email });
